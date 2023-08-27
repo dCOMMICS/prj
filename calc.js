9bc0faf71f5f696c081bb0354615d3d7c40725e6
dCOMMICS <sanwurld@gmail.com>
2023-08-27 14:10:53 +0300

    javascipt simple calculator

---
+++ calc.js
@@ -0,0 +1,25 @@
+function add(){
+    var num1 = parseFloat (document.getElementById ("num1").value);
+    var nm2 = parseFloat (document.getElement ("num2").value);
+    document.getElementById ("result").textContent = num1 + num2;
+}
+
+function subtract(){
+    var num1 = parseFloat (document.getElementById ("num1").value);
+    var nm2 = parseFloat (document.getElement ("num2").value);
+    document.getElementById ("result").textContent = num1 - num2;
+}
+function multiply(){
+    var num1 = parseFloat (document.getElementById ("num1").value);
+    var nm2 = parseFloat (document.getElement ("num2").value);
+    document.getElementById ("result").textContent = num1 * num2;
+}
+function divide(){
+    var num1 = parseFloat (document.getElementById ("num1").value);
+    var nm2 = parseFloat (document.getElement ("num2").value);
+    document.getElementById ("result").textContent = num1 / num2;
+}
+
+
+
+
