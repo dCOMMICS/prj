2857b9f3d2c163eb31cfe466d337dc6a3acbfa8d
dCOMMICS <sanwurld@gmail.com>
2023-08-27 14:34:26 +0300

    javascript side of a complex calculator

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(first)
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const a = Number(formData.get("a"));
+        const b = Number(formData.get("b"));
+
+        result.textContent = (()=>{
+            switch (e.submitter.value){
+                case "summ":
+                    return `summ result: ${a + b}`;
+                    case "subtract":
+                        return `subtract result: ${a - b}`;
+                        case "Multiply":
+                            return `multiply result: ${a * b}`;
+                            case "divide":
+                                return `divide result: ${a  / b}`;
+            }
+        })()
+    }); //  end submit  submit  button  on  submit  button  submit
